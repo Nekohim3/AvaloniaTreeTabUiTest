2a983917e2df7900c706d186ab16361c9fb0834e
Neko <kuroneko@protonmail.ch>
2022-10-10 21:41:38 +0300

    Init

---
+++ AvaloniaTreeTabUiTest/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaTreeTabUiTest.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
